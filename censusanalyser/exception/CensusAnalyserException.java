a4eac9d3f187bb1669f8e350a0001b015c942fea
kamurudivya <93344763+kamurudivya@users.noreply.github.com>
2022-01-09 22:11:19 +0530

    REFACTORED Code To Achieve Adapter Pattern

---
+++ censusanalyser/exception/CensusAnalyserException.java
@@ -0,0 +1,21 @@
+package com.bridgelabz.censusanalyser.exception;
+
+public class CensusAnalyserException extends Exception {
+
+    public ExceptionType type;
+
+    public CensusAnalyserException(String message, ExceptionType type) {
+        super(message);
+        this.type = type;
+    }
+
+    public CensusAnalyserException(String message, String name) {
+        super(message);
+        this.type = ExceptionType.valueOf(name);
+    }
+
+    public enum ExceptionType {
+        CENSUS_FILE_PROBLEM, JSON_WRITE_ERROR, RUN_TIME_EXCEPTION,
+        STATE_CODE_FILE_PROBLEM, INVALID_COUNTRY, CSV_FILE_PROBLEM, NO_CENSUS_DATA
+    }
+}
